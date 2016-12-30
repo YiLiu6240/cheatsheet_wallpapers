565e611f87075312702ef99fa3dd4752ccc44313
YiLiu6240 <yi.liu6240@gmail.com>
2016-12-30 20:18:43 +0000

    add make script

---
+++ others/QuantEcon/make.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+my_file=$1
+my_dir=~/Dropbox/wallpapers/cheatsheets/
+convert -density 300 $my_file.pdf -quality 90 -resize 1920x1080 $my_file.png
+mv ${my_file}-*.png ~/Dropbox/wallpapers/cheatsheets/
