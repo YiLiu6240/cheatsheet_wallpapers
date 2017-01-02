279b03e6c98bbeda429e58a3b2529afb4560d6e4
YiLiu6240 <yi.liu6240@gmail.com>
2017-01-02 12:46:06 +0000

    minor tweak

--- semi-self/QuantEcon/make.sh
+++ semi-self/QuantEcon/make.sh
@@ -3,4 +3,4 @@
 my_file=$1
 my_dir=~/Dropbox/wallpapers/cheatsheets/
 convert -density 300 $my_file.pdf -quality 90 -resize 1920x1080 $my_file.png
-mv ${my_file}-*.png ~/Dropbox/wallpapers/cheatsheets/
+mv ${my_file}-*.png $my_dir
